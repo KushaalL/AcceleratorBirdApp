91aed2d857c22938c8c20a2add943a74bd8c36be
KL School <klatupalli@gmail.com>
2022-04-27 12:10:48 -0400

    go

--- app/src/main/java/com/example/acceleratorbirdproject/MainActivity.java
+++ app/src/main/java/com/example/acceleratorbirdproject/MainActivity.java
@@ -10,22 +10,32 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     //Code from this program has been used from Beginning Android Games
     //Review SurfaceView, Canvas, continue
 
     GameSurface gameSurface;
-
+    Bitmap bird;
+    int birdLeft = 50;
+    int birdTop = 700;
+    boolean goingUp =false;
+    boolean goingDown = false;
+    int z = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         gameSurface = new GameSurface(this);
         setContentView(gameSurface);
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor orientationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this,orientationSensor,SensorManager.SENSOR_DELAY_FASTEST);
+
     }
 
     @Override
@@ -40,6 +50,34 @@ public class MainActivity extends AppCompatActivity {
         gameSurface.resume();
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+//        int x = (int)sensorEvent.values[0];
+        z = (int)sensorEvent.values[0];
+//        int y = (int)sensorEvent.values[1];
+//        Log.d("Tag",x+" "+y+" "+z);
+        if(z>0)
+        {
+            goingUp=true;
+            goingDown=false;
+        }
+        else if(z<0)
+        {
+            goingDown=true;
+            goingUp=false;
+        }
+        else
+        {
+            goingUp=false;
+            goingDown=false;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 
 
     //----------------------------GameSurface Below This Line--------------------------
@@ -49,8 +87,9 @@ public class MainActivity extends AppCompatActivity {
         Thread gameThread;
         SurfaceHolder holder;
         volatile boolean running = false;
-        Bitmap bird;
+
         Bitmap background;
+        Bitmap backgroundScaled;
         Paint paintProperty;
 
         int screenWidth;
@@ -60,8 +99,8 @@ public class MainActivity extends AppCompatActivity {
             super(context);
             holder=getHolder();
             background=BitmapFactory.decodeResource(getResources(),R.drawable.background);
+            backgroundScaled = Bitmap.createScaledBitmap(background,1080,2100,false);
             bird= BitmapFactory.decodeResource(getResources(),R.drawable.bird);
-
             Display screenDisplay = getWindowManager().getDefaultDisplay();
             Point sizeOfScreen = new Point();
             screenDisplay.getSize(sizeOfScreen);
@@ -82,9 +121,10 @@ public class MainActivity extends AppCompatActivity {
                     continue;
                 // https://developer.android.com/reference/android/graphics/Canvas
                 Canvas canvas= holder.lockCanvas();
-                canvas.drawBitmap( bird,200,200,null);
-
+                canvas.drawBitmap(backgroundScaled,0,0,null);
+                canvas.drawBitmap( bird,birdLeft,birdTop,null);
                 holder.unlockCanvasAndPost(canvas);
+                birdTop-=(z*2);
             }
         }
 
